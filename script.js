92d631cd9a7076b95ba1eee4754416ba77d6e78f
yash-bele <yashgbele@gmail.com>
2022-09-29 10:00:09 -0700

    commit

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const ul2_res = document.querySelectorAll(".ul2-res");
 
 nav_menu.addEventListener("click", () => {
     let ht1_val = getComputedStyle(ul1_res).height;
-    ht1_val === "0px" ? ul1_res.style.height = "269px" : ul1_res.style.height = "0px";
+    ht1_val === "0px" ? ul1_res.style.height = "auto" : ul1_res.style.height = "0px";
 });
 
 dd_links.forEach((i, j) => {
@@ -15,21 +15,17 @@ dd_links.forEach((i, j) => {
         if (ht2_val === "0px" && j === 0) {
             ul2_res[1].style.height = "0px";
             ul2_res[2].style.height = "0px";
-            ul2_res[j].style.height = "90px";
-            ul1_res.style.height = String(296 + 65) + "px";
+            ul2_res[j].style.height = "auto";
         } else if (ht2_val === "0px" && j === 1) {
             ul2_res[0].style.height = "0px";
             ul2_res[2].style.height = "0px";
-            ul2_res[j].style.height = "90px";
-            ul1_res.style.height = String(296 + 65) + "px";
+            ul2_res[j].style.height = "auto";
         } else if (ht2_val === "0px" && j === 2) {
             ul2_res[0].style.height = "0px";
             ul2_res[1].style.height = "0px";
-            ul2_res[j].style.height = "60px";
-            ul1_res.style.height = String(296 + 35) + "px";
+            ul2_res[j].style.height = "auto";
         } else {
             ul2_res[j].style.height = "0px";
-            ul1_res.style.height = String(296 - 25) + "px";
         }
     });
 });
